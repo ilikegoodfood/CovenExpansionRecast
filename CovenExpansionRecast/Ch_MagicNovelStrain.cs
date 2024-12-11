7f97ccab7fc74f9f51d3c8d2b96ac52388db71da
ilikegoodfood <ilikegoodfood@googlemail.com>
2024-12-11 20:15:27 +0000

    Magic Plague Novel Strain and Squash Frog
    - Implemented `Ch_MagcNovelStrain`.
    - Implemented `Ch_SquashFrog`.
    - - `getNegativeTags` now returns  the Tags of the person who is the frog.

---
+++ CovenExpansionRecast/Ch_MagicNovelStrain.cs
@@ -0,0 +1,126 @@
+﻿using Assets.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CovenExpansionRecast
+{
+    public class Ch_MagicNovelStrain : Challenge
+    {
+        public Pr_MagicPlague Plague;
+
+        public Ch_MagicNovelStrain(Location loc, Pr_MagicPlague plague)
+            : base(loc)
+        {
+            Plague = plague;
+        }
+
+        public override string getName()
+        {
+            return "Shatter Lucidity";
+        }
+
+        public override string getDesc()
+        {
+            return "Halves the <b>Lucidity</b> in this and surrounding locations, allowing <b>Psychogenic Illnesses</b> to continue to grow.";
+        }
+
+        public override string getRestriction()
+        {
+            return "Requires <b>Psychogenic Illnesses</b>";
+        }
+
+        public override Sprite getSprite()
+        {
+            return EventManager.getImg("CovenExpansion.Icon_PsychIllness.png");
+        }
+
+        public override int isGoodTernary()
+        {
+            return -1;
+        }
+
+        public override bool valid()
+        {
+            return Plague.charge > 0.0;
+        }
+
+        public override double getProfile()
+        {
+            return map.param.ch_novelstrain_aiProfile;
+        }
+
+        public override double getMenace()
+        {
+            return map.param.ch_novelstrain_aiMenace1;
+        }
+
+        public override double getComplexity()
+        {
+            return map.param.ch_novelstrain_complexity;
+        }
+
+        public override challengeStat getChallengeType()
+        {
+            return challengeStat.LORE;
+        }
+
+        public override double getProgressPerTurnInner(UA unit, List<ReasonMsg> msgs)
+        {
+            double val = unit.getStatLore();
+            if (val > 0)
+            {
+                msgs?.Add(new ReasonMsg("Stat: Lore", val));
+                return val;
+            }
+            val = 1.0;
+            msgs?.Add(new ReasonMsg("Base", val));
+
+            return val;
+        }
+
+        public override int getCompletionProfile()
+        {
+            return map.param.ch_novelstrain_completionProfile;
+        }
+
+        public override int getCompletionMenace()
+        {
+            return map.param.ch_novelstrain_completionMenace;
+        }
+
+        public override void complete(UA u)
+        {
+            foreach (Property property in location.properties)
+            {
+                if (property is Pr_Lucidity)
+                {
+                    property.charge *= 0.5;
+                }
+            }
+
+            foreach (Location neighbour in location.getNeighbours())
+            {
+                foreach (Property property in neighbour.properties)
+                {
+                    if (property is Pr_Lucidity)
+                    {
+                        property.charge *= 0.5;
+                    }
+                }
+            }
+        }
+
+        public override int[] buildPositiveTags()
+        {
+            return new int[]
+            {
+                Tags.CRUEL,
+                Tags.DISEASE
+            };
+        }
+    }
+}
---
+++ CovenExpansionRecast/Ch_SquashFrog.cs
@@ -0,0 +1,137 @@
+﻿using Assets.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CovenExpansionRecast
+{
+    public class Ch_SquashFrog : Challenge
+    {
+        public Person Person;
+
+        public Ch_SquashFrog(Location loc, Person p)
+            : base(loc)
+        {
+            Person = p;
+        }
+
+        public override string getName()
+        {
+            if (Person != null)
+            {
+                return $"Squash {Person.getName()}";
+            }
+
+            return "Squash a Frog";
+        }
+
+        public override string getDesc()
+        {
+            if (Person != null)
+            {
+                return $"Squashes {Person.getName()}, killing them.";
+            }
+
+            return "Squashes someone, killing them.";
+        }
+
+        public override string getCastFlavour()
+        {
+            return "All the carefully thought out layers of defense only apply when the target is human. As a frog all that is required is proper timing and a well laced boot.";
+        }
+
+        public override string getRestriction()
+        {
+            return "Requires a frog to be present";
+        }
+
+        public override Sprite getSprite()
+        {
+            return EventManager.getImg("CovenExpansion.Icon_FrogCurse.png");
+        }
+
+        public override int isGoodTernary()
+        {
+            return -1;
+        }
+
+        public override bool valid()
+        {
+            return location.person() != null &&  location.person() == Person;
+        }
+
+        public override double getComplexity()
+        {
+            return 10;
+        }
+
+        public override challengeStat getChallengeType()
+        {
+            return challengeStat.INTRIGUE;
+        }
+
+        public override double getProgressPerTurnInner(UA unit, List<ReasonMsg> msgs)
+        {
+            double val = unit.getStatIntrigue();
+            if (val > 0)
+            {
+                msgs?.Add(new ReasonMsg("Stat: Intrigue", val));
+                return val;
+            }
+
+            val = 1.0;
+            msgs?.Add(new ReasonMsg("Base", val));
+
+            return val;
+        }
+
+        public override int getCompletionProfile()
+        {
+            return 3;
+        }
+
+        public override int getCompletionMenace()
+        {
+            return 4;
+        }
+
+        public override void complete(UA u)
+        {
+            if (Person == null)
+            {
+                return;
+            }
+
+            // Person.die handles the proper removal of the unit, and ruler clearance.
+            u.person.statistic_kills++;
+            Person.die($"Squashed by {u.getName()}", true, u.person);
+
+            if (u.location.settlement != null)
+            {
+                u.location.settlement.customChallenges.Remove(this);
+            }
+        }
+
+        public override int[] buildPositiveTags()
+        {
+            return new int[]
+            {
+                Tags.COMBAT,
+                Tags.CRUEL
+            };
+        }
+
+        public override int[] getNegativeTags()
+        {
+            if (Person != null)
+            {
+                return Person.getTags();
+            }
+
+            return new int[0];
+        }
+    }
+}
